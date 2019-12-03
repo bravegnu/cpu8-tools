6b7a81c22fb9cf7f9590525498cf05fae3ecdb1b
Vijay Kumar B <vijaykumar@zilogic.com>
2019-12-03 11:13:46 +0530

    Added assembler.

---
+++ tools/asm8.py
@@ -0,0 +1,176 @@
+"""Assembler for an 8-bit CPU described in
+
+https://minnie.tuhs.org/CompArch/Tutes/week03.html
+"""
+
+from enum import IntEnum
+
+import re
+import sys
+
+MNEMONIC_LIST = ("AND OR ADD SUB LW SW MOV NOP "
+                 "JEQ JNE JGT JLT LWI SWI LI JMP").split()
+
+REG_LIST = "R0 R1 R2 R3".split()
+
+SYNTAX = re.compile(r"^((?P<label>\w+):)?"
+                    r"(\s*(?P<mnemonic>\w+)"
+                    r"(\s+(?P<operand1>\w+)\s*"
+                    r"(,\s*\(?(?P<operand2>\w+)\)?)?)?)?"
+                    r"\s*([;#].*)?$")
+
+Mnemonics = IntEnum("Mnemonics", zip(MNEMONIC_LIST, range(16)))
+
+
+def error(msg):
+    """Prints an error message and exits."""
+
+    print("asm8: {}".format(msg))
+    exit(1)
+
+
+def parse_reg(reg_name, line):
+    """Returns the register index.
+
+    Exits with error message if the register is invalid.
+    """
+    try:
+        return REG_LIST.index(reg_name)
+    except ValueError:
+        error("invalid register {} in line {}".format(reg_name, line))
+
+
+def parse_mnemonic(mnemonic, line):
+    """Returns the opcode corresponding to the mnemoic.
+
+    Exits with an error message if the mnemonic is invalid.
+    """
+    try:
+        mnemonic = mnemonic.upper()
+        return Mnemonics[mnemonic]
+    except KeyError:
+        error("invalid mnemonic {} in line {}".format(mnemonic, line))
+
+
+def has_imm(opcode):
+    """Returns True if the opcode has an immediate operand."""
+
+    return bool(opcode & 0b1000)
+
+
+def mk_inst(opcode, rd, rs, imm):
+    """Returns the bytes corresponding to the parsed instruction."""
+
+    inst = []
+    inst.append(opcode << 4 | rd << 2 | rs)
+    if has_imm(opcode):
+        inst.append(imm)
+    return bytes(inst)
+
+
+def parse_imm(imm, symtab, line):
+    """Parses immediate value, could be an integer or a label."""
+
+    try:
+        imm = int(imm, 0)
+        if imm > 255:
+            error("invalid immediate value {} in line {}".format(imm, line))
+    except ValueError:
+        try:
+            imm = symtab[imm]
+        except KeyError:
+            error("invalid label {} in line {}".format(imm, line))
+
+    return imm
+
+
+def pass_one(prog, start_addr):
+    """Returns the symbol table for the program."""
+    symtab = {}
+    next_addr = start_addr
+
+    for i, line in enumerate(prog):
+        addr = next_addr
+        m = SYNTAX.match(line)
+        if not m:
+            error("parse error in line {}".format(i))
+
+        label = m.group("label")
+        mnemonic = m.group("mnemonic")
+
+        if label is not None:
+            symtab[label] = addr
+
+        if mnemonic is None:
+            continue
+
+        opcode = parse_mnemonic(mnemonic, i)
+
+        if has_imm(opcode):
+            next_addr += 2
+        else:
+            next_addr += 1
+
+    return symtab
+
+
+def pass_two(prog, symtab):
+    """Parses the asm program and generate binary file."""
+    byte_list = []
+
+    for i, line in enumerate(prog):
+        m = SYNTAX.match(line)
+        mnemonic = m.group("mnemonic")
+        operand1 = m.group("operand1")
+        operand2 = m.group("operand2")
+
+        if mnemonic is None:
+            continue
+
+        opcode = 0
+        dreg = 0
+        sreg = 0
+        imm = None
+
+        opcode = parse_mnemonic(mnemonic, i)
+
+        if operand1 is not None:
+            if opcode == Mnemonics.JMP:
+                imm = parse_imm(operand1, symtab, i)
+            else:
+                operand1 = operand1.upper()
+                dreg = parse_reg(operand1, i)
+
+        if operand2 is not None:
+            if has_imm(opcode):
+                imm = parse_imm(operand2, symtab, i)
+            else:
+                operand2 = operand2.upper()
+                sreg = parse_reg(operand2, i)
+
+        inst = mk_inst(opcode, dreg, sreg, imm)
+        byte_list.append(inst)
+
+    return b"".join(byte_list)
+
+
+def assemble(lines):
+    """Returns the assembled bytes for give source."""
+    symtab = pass_one(lines, 0)
+    return pass_two(lines, symtab)
+
+
+def main(prog_filename, bin_filename):
+    """Main application entry point."""
+
+    try:
+        with open(bin_filename, "wb") as bin_file:
+            with open(prog_filename, "r") as prog_file:
+                lines = prog_file.readlines()
+                binary = assemble(lines)
+                bin_file.write(binary)
+    except OSError as exc:
+        error("error opening file: {}".format(exc))
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
---
+++ tools/test_asm8.py
@@ -0,0 +1,77 @@
+import pytest
+
+from unittest.mock import Mock
+from io import BytesIO
+from io import StringIO
+
+from asm8 import assemble
+from asm8 import main as asm8_main
+
+
+test_valid_insts = [
+    ("AND R0, R0",           b"\x00"),         # Single Byte Instruction
+    ("JMP 0x00",             b"\xF0\x00"),     # Two Byte Instruction
+    ("SW R1, (R1)",          b"\x55"),         # Indirect Addressing
+    ("",                     b""),             # No instructions
+    ("label:",               b""),             # Only label
+    ("nop",                  b"\x70"),         # Lower case instruction
+    ("t: jmp t",             b"\xF0\x00"),     # Jump to self
+    ("# Comment",            b""),             # Only comment
+    ("label: # Comment",     b""),             # Only label and comment
+    ("label: NOP # Comment", b"\x70"),         # Label Mnemonic and Comment
+    ("NOP\n" * 3,            b"\x70\x70\x70"), # Instruction sequence
+    ("b: NOP\njmp b",        b"\x70\xF0\x00"), # Jump Backward
+    ("jmp f\nf: NOP",        b"\xF0\x02\x70"), # Jump Forward
+    ("LWI R1, 0x01",         b"\xC4\x01"),     # Immediate second operand
+    ("LWI R1, 10",           b"\xC4\x0A"),     # Immediate in decimal format
+    ("LWI R1, 0b01010101",   b"\xC4\x55"),     # Immediate in binary format
+]
+
+
+@pytest.mark.parametrize("inst,expected_bin", test_valid_insts)
+def test_valid_assemble(inst, expected_bin):
+    got_bin = assemble(inst.split("\n"))
+    
+    assert got_bin == expected_bin
+
+
+test_invalid_insts = [
+    ("XYZ",            "invalid mnemonic"),
+    ("jmp label",      "invalid label"),
+    ("ADD R4, R4",     "invalid register"),
+    ("LWI R0, 0xFFFF", "invalid immediate"),
+    ("!",              "parse error"),
+]
+
+
+@pytest.mark.parametrize("inst,error_msg", test_invalid_insts)
+def test_invalid_assemble(inst, error_msg, capfd):
+    with pytest.raises(SystemExit):
+        assemble(inst.split("\n"))
+
+    out, err = capfd.readouterr()
+    assert error_msg in out
+
+
+class XBytesIO(BytesIO):
+    def close(self):
+        self.value_at_close = self.getvalue()
+        super().close()
+
+
+@pytest.fixture
+def bin_file(monkeypatch):
+    import builtins
+
+    bin_file = XBytesIO()
+    asm_file = StringIO("nop")
+    mock_open = Mock(side_effect=[bin_file, asm_file])
+    monkeypatch.setattr(builtins, "open", mock_open)
+
+    return bin_file
+
+
+def test_main(bin_file):
+    asm8_main("hello.s", "hello.bin")
+
+    assert bin_file.value_at_close == b"\x70"
